3070a3c89025c50c2de47903c8f601a6b5508e17
valad47 <valad.racz@gmail.com>
2025-09-10 01:22:58 +0300

    Add file deletion and fle control via fd

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/file.h>
+#include <fcntl.h>
 #include "lualib.h"
 
 int vlm_ReadFile(lua_State* L) {
@@ -41,6 +43,11 @@ int vlm_WriteFile(lua_State* L) {
     return 0;
 }
 
+int vlm_DeleteFile(lua_State *L) {
+    remove(luaL_checkstring(L, 1));
+    return 0;
+}
+
 char *str = NULL;
 
 int vlm_system(lua_State* L) {
@@ -56,6 +63,12 @@ int vlm_system(lua_State* L) {
 int vlm_write(lua_State* L) {
     for(int i = 1; i <= lua_gettop(L); i++)
         printf("%s", luaL_checkstring(L, i));
+    return 0;
+}
+
+int vlm_flush(lua_State* L) {
+    fflush(NULL);
+
     return 0;
 }
 
@@ -76,6 +89,51 @@ int vlm_shcall(lua_State* L) {
     lua_pushlstring(L, buff, totalsize);
     free(buff);
     pclose(fp);
+
+    return 1;
+}
+
+
+int vlm_fopen(lua_State* L) {
+    int fd = open(luaL_checkstring(L, 1), O_CREAT|O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    flock(fd, LOCK_EX);
+
+    lua_pushinteger(L, fd);
+    return 1;
+}
+
+int vlm_fread(lua_State *L) {
+    int fd = luaL_checkinteger(L, 1);
+    int fsize = lseek(fd, 0, SEEK_END);
+    lseek(fd, 0, SEEK_SET);
+    char *buff = malloc(fsize + 16);
+    memset(buff, 0, fsize+16);
+
+    int size = read(fd, buff, fsize);
+    if (size == 0) {
+        free(buff);
+        return 0;
+    }
+    lseek(fd, 0, SEEK_SET);
+    lua_pushlstring(L, buff, size);
+    free(buff);
+    return 1;
+}
+
+int vlm_fwrite(lua_State *L) {
+    int fd = luaL_checkinteger(L, 1);
+    size_t size;
+    const char* data = luaL_checklstring(L, 2, &size);
+
+    write(fd, data, size);
+    return 0;
+
+}
+
+int vlm_fclose(lua_State *L) {
+    int fd = luaL_checkinteger(L, 1);
+    flock(fd, LOCK_UN);
+    lua_pushinteger(L, close(fd));
 
     return 1;
 }
